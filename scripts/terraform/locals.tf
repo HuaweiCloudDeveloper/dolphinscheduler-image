190cc543660d51627d73cda21c353551bfa95b7b
Administrator <415212048@qq.com>
2025-06-12 11:44:30 +0800

    Add the deployment usage guide and related script files

---
+++ scripts/terraform/locals.tf
@@ -0,0 +1,70 @@
+locals {
+  // you need to specify a unique name under a tenant according to the business, which will be used as part of the resource name
+  app_id = format("%s-%s", "app", formatdate("hhmm", timestamp()))
+
+  name_suffix = "mkp"
+
+  // Tags of HUAWEI CLOUD resources. You can add tags to resources to classify resources.
+  // for more details, please refer to https://support.huaweicloud.com/usermanual-tms/zh-cn_topic_0056266263.html
+  tags = { Purpose = "MkpApplication" }
+
+
+
+  # Configuration of the ECS memory size and number of cores
+  # instance_flavor_cpu    = 4
+  # instance_flavor_memory = 8
+  #  通用计算增强型
+  instance_performance_type = "computingv3"
+  # 系统盘: 通用SSD
+  ecs_volume_type = "GPSSD"
+
+  # 规格：通用入门型
+  #ecs_flavor = "c7n.xlarge.2"
+
+  // Billing model for cloud resources, You need to modify it according to your actual situation.
+  // In the development and testing phase, pay-per-use billing is recommended.
+  // You can also set these three parameters as variables, allowing users to select at deployment time.
+  charging_mode = var.charging_mode
+  period_unit   = var.period_unit
+  period        = var.period
+
+  // The billing model for bandwidth, You need to modify it according to your actual situation.
+  publicip_type         = "5_bgp"     # 全动态
+  bandwidth_share_type  = "PER"       # 独享带宽
+  bandwidth_charge_mode = "bandwidth" # 按带宽计费
+  bandwidth_size        = 10          # 带宽大小
+
+  # Image information in different regions, you need to enter your own image ID or add another region.
+  # For Marketplace Image Id,you can log in to Seller Console, view the marketplace image id on Product Specifications section of My Products detail page.
+  # 镜像版本：
+  instance_image_id_maps_v1 = {
+#     北京4
+    cn-north-4     = "80989128-d151-4c41-b259-3146e5bf1a8c"
+#     广州
+    cn-south-1     = "f57b9ece-1c79-49e6-843b-0de2d0a3e171"
+#     上海一
+    cn-east-3      = "a606c75f-3fa5-4f48-8d4f-917900057319"
+#     乌兰察布一
+    cn-north-9     = "186fd716-1887-48cd-83d1-72bcf609763b"
+#     贵阳一
+    cn-southwest-2 = "f4efddb2-7bc6-4ae8-8a68-1cbb81ab4f66"
+
+  }
+  # # 其他版本增加（注意修改var参数和镜像的版本的判断部分）
+  #  instance_image_id_maps = {
+  #   #     北京4
+  #   cn-north-4 = ""
+  #  }  
+
+  # Specifies the DNS server address list of a subnet. For details about the private DNS address, see https://support.huaweicloud.com/dns_faq/dns_faq_002.html#?
+  subnet_dns_list_maps = {
+    cn-north-4     = ["100.125.1.250", "100.125.129.250"]
+    cn-south-1     = ["100.125.1.250", "100.125.136.29"]
+    cn-east-3      = ["100.125.1.250", "100.125.64.250"]
+    cn-north-9     = ["100.125.1.250", "100.125.107.250"]
+    cn-southwest-2 = ["100.125.1.250", "100.125.129.250"]
+  }
+
+
+
+}
